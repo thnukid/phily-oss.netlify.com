1af539eeb607546e570eb7acb35015415dabc4e5
Eric Bartholemy <eric@kabisa.nl>
2019-05-20 22:24:18 +0200

    refactor withAuthor hoc

--- src/components/withAuthor.js
+++ src/components/withAuthor.js
@@ -12,14 +12,15 @@ const withAuthor = BaseComponent => {
   return class extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {authors: [], loading: true};
+      this.state = {authors: null, loading: true};
     }
 
     componentDidMount = props => {
       const authors_url = [this.props.data.url, 'stats/contributors'].join('/');
       cachedResponse(authors_url)
         .then(response => {
-          this.setState({authors: response.data, loading: false});
+          let author_items = response.data.sort((a, b) => b.total - a.total);
+          this.setState({authors: author_items, loading: false});
         })
         .catch(function(error) {
           // handle error
@@ -31,32 +32,29 @@ const withAuthor = BaseComponent => {
     render() {
       if (this.state.loading || !this.state.authors) {
         return <div>retrieving authors</div>;
-      } else {
-        let author_items = this.state.authors
-          .sort((a, b) => b.total - a.total)
-          .map((item, index) => (
-            <Author key={index} author={item.author}>
-              <LOCAdded
-                added={item.weeks
-                  .map(item => item.a)
-                  .reduce(
-                    (accumulator, currentValue) => accumulator + currentValue,
-                    0,
-                  )}
-              />{' '}
-              <LOCDeleted
-                deleted={item.weeks
-                  .map(item => item.d)
-                  .reduce(
-                    (accumulator, currentValue) => accumulator + currentValue,
-                    0,
-                  )}
-              />
-            </Author>
-          ));
-
-        return <BaseComponent authors={author_items} {...this.props} />;
       }
+      this.state.authors.map((item, index) => (
+        <Author key={index} author={item.author}>
+          <LOCAdded
+            added={item.weeks
+              .map(item => item.a)
+              .reduce(
+                (accumulator, currentValue) => accumulator + currentValue,
+                0,
+              )}
+          />{' '}
+          <LOCDeleted
+            deleted={item.weeks
+              .map(item => item.d)
+              .reduce(
+                (accumulator, currentValue) => accumulator + currentValue,
+                0,
+              )}
+          />
+        </Author>
+      ));
+
+      return <BaseComponent authors={author_items} {...this.props} />;
     }
   };
 };
