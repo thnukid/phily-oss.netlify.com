8c2f3c4f04e9fee5dbaf68458ccbba90997f9654
Eric Bartholemy <eric@kabisa.nl>
2019-05-21 23:14:22 +0200

    updates footer info

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,12 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer">
     <div className="hero-body has-text-centered">
-      Build by <a href="https://github.com/thnukid">this new kid</a>
+      Build by <a href="https://github.com/thnukid">this new kid</a>, show mf {' '}
+      <a href="https://github.com/thnukid/phily-oss.netlify.com">code</a>
       <hr/>
-      Made with ❤ using Bulma, Gatsby {' '}
+      Made with ❤ using Bulma, Gatsby, Github Api {' '}
+      <br/>
+      2019 Light City Eindhoven
     </div>
   </footer>
 );
