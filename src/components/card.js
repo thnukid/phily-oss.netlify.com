78cbb956abf4ac029268f65fc360cf8e40313fd6
Eric Bartholemy <eric@kabisa.nl>
2019-05-21 20:09:36 +0200

    adds license undefined and title link to repo

--- src/components/card.js
+++ src/components/card.js
@@ -11,15 +11,17 @@ const Card = props => (
             </div>
             <div className="media-content">
               <p className="title is-4">
-                {props.data.description}
-                {' '}
-                <span className="tag is-primary">
-                  {props.data.license && props.data.license.name}
+                <a href={props.data.html_url} target="_blank">{props.data.description}</a>{' '}
+                <span className="tag is-pulled-right is-primary">
+                  {props.data.license ? props.data.license.name : 'Undefined'}
                 </span>
               </p>
               <p className="subtitle is-6">
-                <a href={props.data.html_url}>{props.data.full_name}</a>
+                <a href={props.data.html_url} target="_blank">{props.data.full_name}</a>
               </p>
+              <span class="tag is-light">
+                {props.data.language ? props.data.language : 'Undefined'}
+              </span>
             </div>
           </div>
         </div>
